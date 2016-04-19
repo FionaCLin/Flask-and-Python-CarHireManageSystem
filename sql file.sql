32a8745902774775ca2a59ac46962e7026444801
fiona lin <clin4614@uni.sydney.edu.au>
2016-04-19 15:05:49 +1000

    create car model and location

---
+++ sql file.sql
@@ -0,0 +1,15 @@
+﻿create TYPE location_type as enum('city','suburb','district');
+create table location(
+	id serial primary key,
+	name varchar(50) not null,
+	type location_type,
+	part_of int references location(id)
+);
+
+create table car_model(
+	model_id serial primary key,
+	make varchar(50) not null,
+	model varchar(50) not null,
+	category varchar(25) not null,
+	capacity decimal
+);
