52dd4e37c0a0240955e13cd0a66c21d8fba1f348
FionaCLin <clin4614@uni.sydney.edu.au>
2016-05-22 17:11:51 +1000

    refactor my stored procedure

--- db-app/testing2.sql
+++ db-app/testing2.sql
@@ -7,55 +7,49 @@ DECLARE
 bid INT;
 bn VARCHAR;
 BEGIN
-  bid:=(SELECT bayid FROM carsharing.carbay WHERE name =bname) ;
-  UPDATE carsharing.member SET homebay = bid  
+  bid:=(SELECT bayid FROM CarSharing.Carbay WHERE name =bname) ;
+  UPDATE CarSharing.Member SET homebay = bid  
   WHERE email = e_mail;
-  bn := (SELECT name FROM carsharing.carbay WHERE bayid=bid);
+  bn := (SELECT name FROM CarSharing.Carbay WHERE bayid=bid);
   RETURN bn;
 END;
 $$LANGUAGE 'plpgsql';
-DROP FUNCTION updatehomebay(character varying,character varying)
-SELECT updateHomebay('MrajayBains@gmail.com','Darlinghurst - Crown Street')
 
 --------------------------------------------------------
 
-
-
-CREATE OR REPLACE FUNCTION makeBooking(car_rego VARCHAR,e_mail VARCHAR,date varchar,hour int,duration int)
+CREATE OR REPLACE FUNCTION makeBooking(car_rego VARCHAR,e_mail VARCHAR,date VARCHAR,hour INT,duration INT)
 RETURNS BOOLEAN 
 AS $$
 DECLARE
 member INT;
 stime TIMESTAMP;
 etime TIMESTAMP;
-nrb INT;
 BEGIN 
   stime := (SELECT to_timestamp(date,'YYYY-MM-DD') + hour *interval'1 hour');
-  --add starttime checking constraint in table to forbid member book car in the past
+  --add starttime checking constraINT in table to forbid member book Car in the past
   IF(stime>now()) THEN
     etime := (stime + duration *interval '1 hour');
-    member := (SELECT memberno FROM carsharing.member WHERE email=e_mail);
-    nrb := (SELECT stat_nrofbookings FROM carsharing.member WHERE email = $2);
-    INSERT INTO carsharing.Booking(car,madeby,whenbooked,starttime,endtime)
-    VALUES (car_rego,member,now(),stime,etime);
+    member := (SELECT memberno FROM CarSharing.Member WHERE email=e_mail);
+    INSERT INTO CarSharing.Booking(Car,madeby,whenbooked,starttime,endtime)
+    VALUES (Car_rego,member,now(),stime,etime);
   ELSE
     RAISE EXCEPTION 'No booking made in past';
   END IF;
-  RETURN true;
+  RETURN TRUE;
 END;
 $$LANGUAGE 'plpgsql';
 
 ---check overlapping booking
 CREATE OR REPLACE
 FUNCTION OverlappingTime()
-RETURNS trigger AS $$
+RETURNS TRIGGER AS $$
 DECLARE
 rec RECORD;
 BEGIN
     --refresh my view everytime I need to insert my table.
-    REFRESH MATERIALIZED VIEW CONCURRENTLY carsharing.reservation;
-    --refactor this carsharing.booking to my materialised view reservation
-     FOR rec IN SELECT starttime,  endtime FROM reservation WHERE car = NEW.car
+    REFRESH MATERIALIZED VIEW CONCURRENTLY CarSharing.reservation;
+    --refactor this CarSharing.booking to my materialised view reservation
+    FOR rec IN SELECT starttime,endtime FROM Reservation WHERE car = NEW.car
     LOOP
         IF (rec.starttime, rec.endtime) OVERLAPS (NEW.starttime, NEW.endtime) THEN
             RAISE EXCEPTION 'Overlapping booking';
@@ -65,39 +59,28 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
----triger check overlap trigger and refresh the materialised view---
+---triger check overlap TRIGGER and refresh the materialised view---
 CREATE TRIGGER CheckOverlappingTime
-BEFORE INSERT OR UPDATE ON carsharing.Booking
+BEFORE INSERT OR UPDATE ON CarSharing.Booking
 FOR EACH ROW
 EXECUTE PROCEDURE OverlappingTime();
 
 CREATE OR REPLACE FUNCTION incrementNumberOfBooking()
-RETURNS trigger AS $$
+RETURNS TRIGGER AS $$
 DECLARE
-nrb INT;
+nrb INT ;
 BEGIN
-    nrb := (SELECT stat_nrofbookings FROM carsharing.member WHERE memberno = NEW.madeby);
-    UPDATE carsharing.member SET stat_nrofbookings=nrb+1 WHERE memberno = NEW.madeby;
-    return old;
+    nrb := (SELECT stat_nrofbookings FROM CarSharing.Member WHERE memberno = NEW.madeby);
+    UPDATE CarSharing.Member SET stat_nrofbookings=nrb+1 WHERE memberno = NEW.madeby;
+    RETURN old;
 END;
 $$ LANGUAGE plpgsql;
 
 ---triger update member statistic number of booking ---
 CREATE TRIGGER updateMemberStatOfBooking
-AFTER INSERT ON carsharing.Booking
+AFTER INSERT ON CarSharing.Booking
 FOR EACH ROW
 EXECUTE PROCEDURE incrementNumberOfBooking();
-
-
-SELECT * from makebooking('AT61LA','MrajayBains@gmail.com','2019-05-20',12,4);
-
-SELECT stat_nrofbookings FROM CARSHARING.MEMBER WHERE EMAIL = 'MrajayBains@gmail.com'
-
-delete from carsharing.booking where car='AT61LA' 
-
-SELECT * from carsharing.booking where car='AT61LA'
-
-SELECT (to_timestamp('2017-05-20','YYYY-MM-DD') + 17 *interval'1hour')
 
 --------------------------------------------------------
 
@@ -106,71 +89,55 @@ RETURNS TABLE(reg REGOTYPE,cn VARCHAR)
 AS $$
 BEGIN
  RETURN QUERY SELECT regno, name 
- FROM carsharing.car 
+ FROM CarSharing.Car 
  WHERE parkedat = ( 
- SELECT bayid 
- FROM carsharing.carbay 
- WHERE name = bname);
+  SELECT bayid 
+  FROM CarSharing.Carbay 
+  WHERE name = bname);
 END;
 $$LANGUAGE 'plpgsql';
 
-
-select * from getCarsInBay('Erskineville - Erskineville Road')
-
 --------------------------------------------------------
-CREATE OR REPLACE FUNCTION getAllBooking(e_mail varchar)
-RETURNS table(car regotype,name varchar,date date,hour int,stime timestamp) 
+CREATE OR REPLACE FUNCTION getAllBooking(e_mail VARCHAR)
+RETURNS table(Car REGOTYPE,name VARCHAR,date DATE,hour INT,stime TIMESTAMP) 
 AS $$
 BEGIN
-  Return QUERY SELECT b.car AS car, c.name AS name , 
-  cast(b.starttime as date) 
+  RETURN QUERY
+  SELECT b.Car AS Car, c.name AS name , 
+  CAST(b.starttime AS date) 
   AS date ,
-  cast( EXTRACT(HOUR FROM starttime) as int )AS hour ,b.starttime
-  FROM carsharing.Booking AS b join carsharing.Car As C ON b.car = regno 
-            WHERE b.madeby = (SELECT memberno FROM carsharing.member WHERE email=e_mail)
+  CAST(EXTRACT(EPOCH FROM endtime-starttime) AS int)/3600 AS duration ,b.starttime
+  FROM CarSharing.Booking AS b join CarSharing.Car As C ON b.Car = regno 
+  WHERE b.madeby = (SELECT memberno FROM CarSharing.Member WHERE email=e_mail)
   ORDER BY b.starttime DESC;
 END;
 $$  
 LANGUAGE 'plpgsql'
 
-
-drop function getAllBooking(varchar)
-SELECT * from getAllBooking('MrajayBains@gmail.com') ;
-
 --------------------------------------------------------
-CREATE OR REPLACE FUNCTION fetchBays(searchTerm text)
+CREATE OR REPLACE FUNCTION fetchBays(searchTerm TEXT)
 RETURNS TABLE(name VARCHAR, address VARCHAR, nrOfCar BIGINT)
 AS $$
 BEGIN
   searchTerm := '%'||searchTerm||'%';
   RETURN QUERY SELECT b.name , b.address, 
-  count(c.regno) FROM carsharing.carbay as b JOIN carsharing.car as c
-  ON b.bayid = c.parkedat WHERE b.name ILIKE searchTerm or 
+  COUNT(c.regno) FROM CarSharing.Carbay AS b JOIN CarSharing.Car AS c
+  ON b.bayid = c.parkedat WHERE b.name ILIKE searchTerm OR 
   b.address ILIKE searchTerm
   GROUP BY bayid ;
  END;
  $$LANGUAGE 'plpgsql';
 
-select cast(now() as date)
-
-
-SELECT * from fetchBays('Road') 
-
-
 --------------------------------------------------------
 CREATE OR REPLACE FUNCTION getBay(n VARCHAR)
-RETURNS TABLE( bname VARCHAR, descr text,
+RETURNS TABLE( bname VARCHAR, descr TEXT,
  addr VARCHAR,gpsLat FLOAT,gpsLong FLOAT,walkscore INT) AS $$
 BEGIN
  RETURN QUERY 
  SELECT name ,description,address,gps_lat,gps_long, cb.walkscore
- FROM carbay cb WhERE cb.name =n;
+ FROM Carbay cb WhERE cb.name =n;
  END;
- $$LANGUAGE 'plpgsql';
-
-SELECT *from getBay('Erskineville - Erskineville Road') AS (name ,description, address,gps_lat,gps_long)
-
-DROP FUNCTION getbay(character varying)
+$$LANGUAGE 'plpgsql';
 
 
 --------------------------------------------------------
@@ -178,83 +145,60 @@ CREATE OR REPLACE FUNCTION getAllBays()
 RETURNS TABLE(name VARCHAR,address VARCHAR, nrOfCar BIGINT)
 AS $$
 BEGIN
-  RETURN QUERY SELECT carsharing.carbay.name , carsharing.carbay.address, 
-  count(carsharing.car.regno) FROM carsharing.carbay  JOIN carsharing.car 
+  RETURN QUERY SELECT CarSharing.Carbay.name , CarSharing.Carbay.address, 
+  COUNT(CarSharing.Car.regno) FROM CarSharing.Carbay  JOIN CarSharing.Car 
   ON bayid = parkedat GROUP BY bayid;
  END;
  $$LANGUAGE 'plpgsql';
 
-SELECT * FROM GETALLBAYS();
-
-
-
 --------------------------------------------------------
-CREATE OR REPLACE FUNCTION getCarDetail(rego varchar)
-RETURNS TABLE(regno regotype,name varchar,make varchar,
-model varchar,year int,transmission varchar,category varchar,
-capacity int,bay varchar,walkscore int,mapurl varchar)
+CREATE OR REPLACE FUNCTION getCarDetail(rego VARCHAR)
+RETURNS TABLE(regno regotype,name VARCHAR,make VARCHAR,
+model VARCHAR,year INT,transmission VARCHAR,category VARCHAR,
+capacity INT,bay VARCHAR,walkscore INT,mapurl VARCHAR)
 AS $$
 BEGIN
   RETURN QUERY SELECT c.regno,c.name, c.make, c.model, c.year,c.transmission, 
   m.category,m.capacity, b.name, b.walkscore,b.mapurl 
-  FROM carsharing.car AS c NATURAL JOIN carsharing.carmodel as m
-  JOIN carsharing.carbay AS b ON parkedat=bayid WHERE c.regno = rego;
+  FROM CarSharing.Car AS c NATURAL JOIN CarSharing.Carmodel AS m
+  JOIN CarSharing.Carbay AS b ON parkedat=bayid WHERE c.regno = rego;
  END;
  $$LANGUAGE 'plpgsql';
 
-
-Select * From getCarDetail('AN83WT');
-
 --------------------------------------------------------
-CREATE OR REPLACE FUNCTION fetchbooking(b_car char(6),b_date date,b_hour int)
-RETURNS TABLE (
-  mname text, 
-  car regotype,
-  cname varchar,
-  date date,
-  hour int,
-  duration int,
-  madeday text,
-  bay varchar,
-  cost float)
+CREATE OR REPLACE FUNCTION fetchbooking(b_car CHAR(6),b_date DATE,b_hour INT)
+RETURNS TABLE ( mname TEXT, car regotype, cname VARCHAR, date DATE,
+  hour INT,duration INT,madeday TEXT,bay VARCHAR,cost FLOAT)
 AS $$
 BEGIN
   RETURN QUERY 
- SELECT m.namegiven||' '||m.namefamily, b.car, c.name, 
-    cast(b.starttime as date) AS date, 
-    cast(EXTRACT(HOUR FROM starttime) as int) as hour ,
-    cast(EXTRACT(EPOCH FROM endtime-starttime) as int)/3600 AS duration,
-    to_char(b.whenbooked,'DD-MM-YYYY')  AS madeday , cb.name as bay 
-    , cast(EXTRACT(EPOCH FROM endtime-starttime) as float)/3600*(
-      SELECT hourly_rate FROM carsharing.membershipplan
+ SELECT m.namegiven||' '||m.namefamily, b.Car, c.name, 
+    CAST(b.starttime AS date) AS date, 
+    CAST(EXTRACT(HOUR FROM starttime) AS int) AS hour ,
+    CAST(EXTRACT(EPOCH FROM endtime-starttime) AS int)/3600 AS duration,
+    to_char(b.whenbooked,'DD-MM-YYYY')  AS madeday , cb.name AS bay 
+    , CAST(EXTRACT(EPOCH FROM endtime-starttime) AS FLOAT)/3600*(
+      SELECT hourly_rate FROM CarSharing.Membershipplan
       WHERE title =m.subscribed 
-    ) as cost
-    FROM carsharing.booking AS b 
-      JOIN carsharing.car AS C ON b.car=regno 
-      JOIN carsharing.member AS m ON b.madeby= m.memberno 
-      JOIN carsharing.carbay as cb ON c.parkedat=cb.bayid 
-    WHERE b.car=$1 AND cast(b.starttime as date) =$2 
-      AND EXTRACT(HOUR FROM starttime) = $3;
-
-
+    ) AS cost
+    FROM CarSharing.booking AS b 
+      JOIN CarSharing.Car AS C ON b.Car=regno 
+      JOIN CarSharing.Member AS m ON b.madeby= m.Memberno 
+      JOIN CarSharing.Carbay AS cb ON c.parkedat=cb.bayid 
+    WHERE b.Car=b_Car AND CAST(b.starttime AS date) =b_date
+      AND CAST(EXTRACT(EPOCH FROM endtime-starttime) AS int)/3600  = b_hour;
 END;
 $$ LANGUAGE 'plpgsql'
 
-DROP FUNCTION fetchbooking(character,character varying,integer)
-
-SELECT * FROM fetchbooking('AN83WT','3424-02-03',17) ;
 
 
-CREATE MATERIALIZED VIEW carsharing.Reservation
+CREATE MATERIALIZED VIEW CarSharing.Reservation
 AS
- SELECT car,starttime,endtime
-   FROM carsharing.booking
-   WHERE starttime > now()
-   order by starttime desc
-  with data;
+  SELECT Car,starttime,endtime
+  FROM CarSharing.booking
+  WHERE starttime > now()
+  ORDER BY starttime DESC
+WITH DATA;
 
-CREATE UNIQUE INDEX DATE_TIME ON RESERVATION (car,starttime);
+CREATE UNIQUE INDEX DATE_TIME ON RESERVATION (Car,starttime);
 
-SELECT car,starttime,  endtime FROM carsharing.Reservation --WHERE car ='AT61LA'
-
-DROP MATERIALIZED VIEW Reservation
